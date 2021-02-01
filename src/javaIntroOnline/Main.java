983fbd17843eeea1fe04a198a0a27eabf7790a25
Franc Metlevski <frantcishakm@gmail.com>
2021-02-01 13:19:40 +0300

    first task

---
+++ src/javaIntroOnline/Main.java
@@ -0,0 +1,12 @@
+package javaIntroOnline;
+
+public class Main {
+	public static void main(String[] args) {
+		int a =4;
+		int b =4;
+		int c =6;
+		int z =((a-3)*b/2)+c;
+		System.out.println(z);
+	} 
+
+}
