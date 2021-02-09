7e0622b08d77f6817a60c1a5b79956cb2dd74942
Franc Metlevski <frantcishakm@gmail.com>
2021-02-09 10:34:56 +0300

    second task

---
+++ src/javaIntroOnline/PracticeTask2.java
@@ -0,0 +1,11 @@
+package javaIntroOnline;
+
+public class PracticeTask2 {
+	public static void main(String[] args) {
+		double a =4;
+		double b =4;
+		double c =6;
+		double z =(b+Math.sqrt((Math.pow(b, 2)+4*a*c))/2*a)-Math.pow(a, 3)*c+Math.pow(b, -2);
+		System.out.println(z);
+	} 
+}
