970185aeb74d60606440bd098077278e3287279c
Franc Metlevski <frantcishakm@gmail.com>
2021-02-12 18:59:57 +0300

    second and fourth branching  tasks

---
+++ src/javaIntroOnline/BranchingTask2.java
@@ -0,0 +1,12 @@
+package javaIntroOnline;
+
+public class BranchingTask2 {
+	public static void main(String[]args) {
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		System.out.println(Math.max(Math.min(a, b),Math.min(c, d)));
+	}
+
+}
---
+++ src/javaIntroOnline/BranchingTask4.java
@@ -0,0 +1,18 @@
+package javaIntroOnline;
+
+public class BranchingTask4 {
+	public static void main(String[]args)
+	{
+		int a=10, b=6; 
+		int x =2, y=44, z=5;
+		if(a>=x&&b>=y||b>=x&&a>=y) 
+		{System.out.println("It`s OK!");}
+		else if(a>=x&&b>=z||b>=x&&a>=z)
+		{System.out.println("It`s OK!");}
+		else if(a>=z&&b>=y||b>=z&&a>=y)
+		{System.out.println("It`s OK!");}
+		else {System.out.println("It does not pass");}
+
+
+	}
+}
