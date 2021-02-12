4d23052f97947d3a2b5e08f69085a559e9c213ca
Franc Metlevski <frantcishakm@gmail.com>
2021-02-12 21:08:05 +0300

    3rd and 5th branching  tasks

---
+++ src/javaIntroOnline/BranchingTask3.java
@@ -0,0 +1,18 @@
+package javaIntroOnline;
+
+public class BranchingTask3 {
+	public static void main(String[]args) {
+	int x1=0;
+	int y1=0;
+	
+	int x2=2;
+	int y2=2;
+	
+	int x3=3;
+	int y3=3;
+	
+		if ((x3-x1)/(x2-x1) == (y3-y1)/(y2-y1)) {System.out.println("point belongs to the line");} 
+		else System.out.println("point does not belong to the line");
+	}
+
+}
---
+++ src/javaIntroOnline/BranchingTask5.java
@@ -0,0 +1,10 @@
+package javaIntroOnline;
+import java.util.Scanner;
+public class BranchingTask5 {
+public static void main(String[]args) {
+	Scanner sc = new Scanner(System.in);
+	double x =sc.nextDouble(),y;
+	if(x<=3) {y=Math.pow(x, 2)+3*x+9; System.out.println(y);}
+	else if(x>3) {y=1/(Math.pow(x, 3)+6); System.out.println(y);} 
+}
+}
