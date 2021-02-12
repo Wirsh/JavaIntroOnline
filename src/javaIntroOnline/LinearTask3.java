e26fac4edd91692fab02aeacf3bbd338afda9585
Franc Metlevski <frantcishakm@gmail.com>
2021-02-12 14:58:28 +0300

    third linear task

---
+++ src/javaIntroOnline/LinearTask3.java
@@ -0,0 +1,12 @@
+package javaIntroOnline;
+public class LinearTask3 {
+	public static void main (String [] args) {
+			double x = 3; 
+			double y = 5; 
+			double z =	((Math.sin(x)+Math.cos(y))/(Math.cos(x)-Math.sin(y)))*Math.tan((x*y));
+			System.out.println(z);
+
+		}
+
+	}
+
